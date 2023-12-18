1e4ec13b61f1fc6128279873fcf19dcf10504f35
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2023-12-18 14:31:06 +0100

    just another commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,39 @@
+import {useState} from "react";
+import Die from "./Die";
+
 export default function Body() {
   let diceArray = [];
-  let numOfDice = parseInt(prompt("How many players do you want?"));
-    if(numOfDice >= 2 && numOfDice <= 100) {
-      for(let i = 1; i <= numOfDice; i++) {
-        diceArray.push(`Die ${i}`);
-      }
-    }
+  const numOfDice = parseInt(prompt("Choose an even number between 1 and 100 for the number of dice"));
+
+  if(numOfDice >= 2 && numOfDice <= 100) {
+    for(let i = 1; i <= numOfDice; i++) {
+      diceArray.push({name: "Die " + [i], value: true});
+    };
+  };
+  
+  // const [toggle, setToggle] = useState(true);
+  // const handleChange = e => {
+
+  }
+
+  const diceMap = diceArray.map(datum => {
+    datum = true
+    return <Die
+    key={datum}
+    onClick={() => {
+      !datum;
+      
+    }}
+    />
+  })
   return (
     <div id="body">
       <p>
         Roll until all dice are the same.Click each die to freeze it at its current value between rolls.
       </p>
-
+      <div id="die-container">
+        {diceMap}
+      </div>
     </div>
   )
 };
