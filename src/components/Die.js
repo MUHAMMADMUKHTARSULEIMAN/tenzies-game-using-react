bf0f815597f0a97a4190fee82b9318b9b869f175
MUHAMMADMUKHTARSULEIMAN <muhammadmukhtarsuleiman@gmail.com>
2023-12-19 12:18:14 +0100

    just another commit

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,5 +1,5 @@
 export default function Die(props) {
-  const styles = props.on ? {backgroundColor: "#FFFFFF"} : {backgroundColor: "#32CD32"};
+  const styles = props.on ? {backgroundColor: "#FFFFFF"} : {backgroundColor: "#5BCD5B"};
 
   return (
     <div style={styles} id="die" onClick={() => props.toggle(props.name)}>
